2d89a74957d8b0a81a4406e88e0da9a2da761274
Thomas Tempel <thomastempel@Thomass-Air.hsd1.il.comcast.net>
2022-12-01 19:39:31 -0600

    saving changes

--- DBZ.Forms/Goku/gokurps.js
+++ DBZ.Forms/Goku/gokurps.js
@@ -1,24 +1,18 @@
 let playerscore=0
 let Gokuscore=0
 
-const Goku = (Gokuchoice) => {
+const Goku = () => {
     const choice = ["rock", "paper", "scissors"]
     const random = Math.floor(Math.random()*3)
     Gokuchoice = choice[random]
     return Gokuchoice
 }
 
-const letsplay =(player, Gokuplay)=> {
-    if (player === "rock" && Gokuplay === "rock"){
-        return ("You tie! Rock loves rock.")
+const letsplay = (player, Gokuplay)=> {
+    if (player === Gokuplay) {
+        return(`You tie! You both chose ${player}`)
     }
-    else if (player === "paper" && Gokuplay=== "paper"){
-        return ("You tie! Paper sticks to paper.") 
-    }
-    else if (player === "scissors" && Gokuplay=== "scissors"){
-        return ("You tie! Scissors like to scissor scissors") 
-    }
-    //The above are all ties and get no points.
+    //The above are ties and get no points.
     else if (player === "rock" && Gokuplay=== "paper"){
         Gokuscore++
         return ("You lose! Paper covers rock.") 
@@ -45,24 +39,20 @@ const letsplay =(player, Gokuplay)=> {
     }
 }
 
-
-
-
-
 const game = () => {
 for (let i = 0; i < 5; i++); {
     const player=prompt("Whats your choice? Rock, paper, or scissors?").toLowerCase();
     const Gokuplay = Goku();
-    letsplay(player,Gokuplay)
     console.log(letsplay(player,Gokuplay));
     }
 
-    if (playerscore===5) {
-        console.log("You win! Goku wants a rematch")
+    if (playerscore>Gokuscore) {
+        console.log("You win the game! Keep training because Goku will be back")
     }
-    else if (Gokuscore===5) {
-        console.log("You lose! Train harder if you want to win.")
+    else if (Gokuscore>playerscore) {
+        console.log("You lose! Keep training you weak bitch.")
     }
 }
 
 game()
+
